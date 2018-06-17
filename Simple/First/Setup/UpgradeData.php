b1d5187835b1532ca8925baf4fdc60e7593d89df
chirag4040 <chiragkadiya4040@yahoo.in>
2018-06-17 10:47:28 +0530

    simple first module commit

---
+++ Simple/First/Setup/UpgradeData.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Simple\First\Setup;
+
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\UpgradeDataInterface;
+ 
+
+class UpgradeData implements UpgradeDataInterface {
+    private $eavSetupFactory;
+ 
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+ 
+
+    public function upgrade( ModuleDataSetupInterface $setup, ModuleContextInterface $context ) {
+ 
+        if ( version_compare( $context->getVersion(), '0.0.2', '<' ) ) {
+            $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+ 
+            $eavSetup->addAttribute(
+                \Magento\Catalog\Model\Product::ENTITY,
+                'custom_attribute',
+                [
+                    'group' => 'General',
+                    'type' => 'int',
+                    'backend' => '',
+                    'frontend' => '',
+                    'label' => 'Custom Label',
+                    'input' => 'boolean',
+                    'class' => '',
+                    'source' => 'Magento\Eav\Model\Entity\Attribute\Source\Boolean',
+                    'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                    'visible' => true,
+                    'required' => false,
+                    'user_defined' => false,
+                    'default' => '1',
+                    'searchable' => false,
+                    'filterable' => false,
+                    'comparable' => false,
+                    'visible_on_front' => false,
+                    'used_in_product_listing' => false,
+                    'unique' => false,
+                    'apply_to' => 'simple,configurable,virtual,bundle,downloadable'
+                ]
+            );
+        }
+    }
+}
